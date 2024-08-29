1fd2a74eef3097c39a4800157cdb26e95cdccaf5
Murilo Teixeira <murilogteixeira@gmail.com>
2024-08-29 00:31:29 -0300

    feat: Initial commit

---
+++ UXMD-Curso/UXMD_CursoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UXMD_CursoApp.swift
+//  UXMD-Curso
+//
+//  Created by Murilo Teixeira on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct UXMD_CursoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
